c7d92ad60edd636b9979f0aa2fae40be2e401bc1
leyicao@gmail.com <leyicao@gmail.com>
2023-10-22 15:52:59 -0700

    refactor about me section

--- src/Pages/AboutMe/index.js
+++ src/Pages/AboutMe/index.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import Button from '@mui/material/Button';
+import React from 'react';
 import Grid from '@mui/material/Grid';
 
 import PageContainer from '../../layouts/PageContainer';
@@ -8,45 +6,16 @@ import './AboutMe.scss'
 
 const TabItems = [
   { tabName: 'Education', header: 'Education', subHeader: '14124 dasd 23', body: '1das d das das das d23' },
-  { tabName: 'Skills', header: 'Skills', subHeader: '14124 dasd 23', body: '1das d das das das d23' },
-  { tabName: 'Experience', header: 'Experience', subHeader: '14124 dasd 23', body: '1das d das das das d23' }
 ]
 
-function CustomTabPanel(props) {
-  const { children, value, selectedValue, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={selectedValue !== value}
-      id={`simple-tabpanel-${value}`}
-      aria-labelledby={`simple-tab-${value}`}
-      {...other}
-    >
-      {children}
-    </div>
-  );
-}
-
 const AboutMePageView = () => {
-  const [activeTab, setActiveTab] = useState(TabItems[0].tabName);
-
   return (
     <PageContainer >
-      <Grid id="AboutMe">
-        <div className='tab-container'>
-          {
-            TabItems.map((item) =>
-              <Button key={item.tabName} className={classNames({
-                active: item.tabName === activeTab,
-                tab: true,
-              })} onClick={() => setActiveTab(item.tabName)}>{item.tabName}</Button>
-            )
-          }
-        </div>
-
+      <Grid className="about-me-section" id="AboutMe">
+        <h2 className='section-header' >About Me</h2>
+        <h1 className='header'>Education</h1>
         {TabItems.map((item) =>
-          <CustomTabPanel value={item.tabName} selectedValue={activeTab}>
+          <div>
             <Grid container>
               <Grid item lg={6} xs={12}>
                 <div className='tab-card'>
@@ -88,7 +57,7 @@ const AboutMePageView = () => {
                 </div>
               </Grid>
             </Grid>
-          </CustomTabPanel>
+          </div>
         )}
       </Grid>
     </PageContainer>
